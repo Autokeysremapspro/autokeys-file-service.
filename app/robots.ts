f395c4eedd56e712562efeffd51dc05ac49d3713
Autokeysremapspro <info@autokeyspro.es>
2026-08-05 11:45:32 +0200

    N

--- app/robots.ts
+++ app/robots.ts
@@ -13,8 +13,6 @@ export default function robots(): MetadataRoute.Robots {
           '/garage',
           '/biblioteca',
           '/soporte',
-          '/creditos',
-          '/comprar-creditos',
           '/admin',
           '/pendiente-aprobacion',
           '/restablecer-contrasena',
