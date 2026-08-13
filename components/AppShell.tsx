408ba3db95abca01cae6385907c8f278c20b2408
Autokeysremapspro <info@autokeyspro.es>
2026-08-13 02:26:03 +0200

    feat: show Phase 2 intro once per session

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import AKPageShell from '@/components/ak/AKPageShell'
+import AKSessionIntro from '@/components/ak/AKSessionIntro'
 import LaboratoryStatusBanner from '@/components/LaboratoryStatusBanner'
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <AKPageShell>
-      <LaboratoryStatusBanner />
-      {children}
-    </AKPageShell>
+    <>
+      <AKSessionIntro />
+      <AKPageShell>
+        <LaboratoryStatusBanner />
+        {children}
+      </AKPageShell>
+    </>
   )
 }
