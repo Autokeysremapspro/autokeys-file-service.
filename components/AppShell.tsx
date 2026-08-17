8e14e169a62cf7037971db2e9a5c357b120f7f66
Autokeysremapspro <info@autokeyspro.es>
2026-08-17 16:31:07 +0200

    Remove AKCloud intro and enter dashboard directly

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import AKPageShell from '@/components/ak/AKPageShell'
-import AKSessionIntro from '@/components/ak/AKSessionIntro'
 import LaboratoryStatusBanner from '@/components/LaboratoryStatusBanner'
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <AKSessionIntro />
       <AKPageShell>
         <LaboratoryStatusBanner />
         {children}
