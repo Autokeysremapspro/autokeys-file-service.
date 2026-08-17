752c1ce7adeb4384c1d37161b9f52afb576e477d
Autokeysremapspro <info@autokeyspro.es>
2026-08-17 10:38:09 +0200

    Assemble complete AKCloud intro image

--- components/ak/intro-image-v5/index.ts
+++ components/ak/intro-image-v5/index.ts
@@ -1,7 +1,8 @@
 import part00 from './part00'
 import part01 from './part01'
 import part02 from './part02'
+import part03 from './part03'
 
-const AKCLOUD_INTRO_IMAGE_V5 = `data:image/webp;base64,${part00}${part01}${part02}`
+const AKCLOUD_INTRO_IMAGE_V5 = `data:image/webp;base64,${part00}${part01}${part02}${part03}`
 
 export default AKCLOUD_INTRO_IMAGE_V5
